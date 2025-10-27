c8c6bee74193787aa3da74d43f7653a823c1afae
dongocanh9 <dongocanh0703@gmail.com>
2025-10-27 12:49:44 +0700

    btvn buoi2
    Fix calculation of e^x and variable naming.

--- BUOI2/Bai1.py
+++ BUOI2/Bai1.py
@@ -6,10 +6,10 @@ for n in range(2,n+1):
     S+=1/math.factorial(n)
 print(S)
 #1.1
-e^x=1
+tong=1
 x=int(input("Nhập vào số nguyên x bất kì: "))
 n=int(input("Nhập váo 1 số nguyên không âm bất kì : "))
 for n in range (2,n+1):
     giaithua=(1/math.factorial(n))*(x**n)
-    e^x+=giaithua
-print(e^x)
+    tong+=giaithua
+print(tong)
