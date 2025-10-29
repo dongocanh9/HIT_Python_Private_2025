220520b14309d5a6a6ad360790da37f86b72f13d
dongocanh9 <dongocanh0703@gmail.com>
2025-10-29 18:35:23 +0700

    btvn buoi2

--- BUOI2/Bai1.py
+++ BUOI2/Bai1.py
@@ -2,14 +2,14 @@ import math
 #1.2
 n=int(input("Nhập váo 1 số nguyên không âm bất kì : "))
 S=1
-for n in range(2,n+1):
-    S+=1/math.factorial(n)
+for i in range(2,n+1):
+    S+=1/math.factorial(i)
 print(S)
 #1.1
 tong=1
 x=int(input("Nhập vào số nguyên x bất kì: "))
 n=int(input("Nhập váo 1 số nguyên không âm bất kì : "))
-for n in range (2,n+1):
-    giaithua=(1/math.factorial(n))*(x**n)
+for i in range (2,n+1):
+    giaithua=(1/math.factorial(i))*(x**i)
     tong+=giaithua
 print(tong)
